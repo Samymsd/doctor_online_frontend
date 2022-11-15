14a8c24f1db5598ba46914866ba9930ab69c5127
Samy David <samyms6@gmail.com>
2022-11-14 20:34:35 -0500

    Configurando proyecto para heroku

--- src/components/services/SpecialtiesService.js
+++ src/components/services/SpecialtiesService.js
@@ -1,42 +1,30 @@
 import axios from "axios";
 
-const specialties = [
-  {
-    name: "Otorrino",
-    description: "Cunsulta los oidos y las vias superiores",
-    attends_only_women: false
-  },
-  {
-    name: "Gninecologia",
-    description: "Cunsulta aparato reproductor femenino",
-    attends_only_women: true
-  }
-];
-
+const URI = "https://doctor-online-api.herokuapp.com/";
 const SpecialtiesService = {};
 
 SpecialtiesService.getSpecialties = ()=>{
-  return axios.get("http://localhost:4000/specialty");
+  return axios.get(URI + "specialty");
 };
 
 SpecialtiesService.specialtiesFindByCriterion = (criterion) => {
-  return axios.get("http://localhost:4000/specialty?q=" + criterion);
+  return axios.get(URI + "specialty?q=" + criterion);
 };
 
 SpecialtiesService.loadSpecialties = (id) => {
-  return axios.get("http://localhost:4000/specialty/" + id);
+  return axios.get(URI + "specialty/" + id);
 };
 
 SpecialtiesService.saveSpecialties = (specialty) => {
-  return axios.post("http://localhost:4000/specialty", specialty);
+  return axios.post(URI + "specialty", specialty);
 };
 
 SpecialtiesService.editSpecialties = (id, specialty) => {
-  return axios.put("http://localhost:4000/specialty/" + id, specialty);
+  return axios.put(URI + "specialty/" + id, specialty);
 };
 
 SpecialtiesService.deleteSpecialties = (id) => {
-  return axios.delete("http://localhost:4000/specialty/" + id);
+  return axios.delete(URI + "specialty/" + id);
 };
 
 export default SpecialtiesService;
